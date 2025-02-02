768d94093f9e6d543a495a3c985fc78ff5f8fbf2
Mohtashim-99 <mohtashimanayat63@gmail.com>
2025-02-02 21:16:34 +0530

    Uploaded  blog page

---
+++ blogs/markhor-in-kashmir.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blogs
+    </title>
+    <!-- /font awesome link / -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/bootstarp.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+
+</head>
+
+<body>
+    <header class="top-header-outer" >
+    </header>
+    <div class="container-fluid position-relative p-0">
+        <div class="navbar-outer position-relative" style="z-index:99999;">
+        </div>
+        <div class="container-fluid py-5 hero-header mb-5">
+            <div class="row justify-content-center py-5 mt-4">
+                <div class="col-lg-10 pt-lg-5 mt-lg-5 text-center">
+                    <h1 class="display-3 text-white ">Blogs</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item"><a href="/blogs">Blogs</a></li>
+                            <li class="breadcrumb-item text-white active">Markhor in Kashmir</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div
+    <div class="container-xxl py-5">
+      
+
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="text-warning mb-2">
+                    Product Updates · FEB 02, 2025
+                </div>
+                <h1 class="display-4 fw-medium mb-4 fs-1" id="markhor-in-kashmir">
+                The Markhor in Kashmir: Ecology, Conservation, and Cultural Significance
+                </h1>
+                <p class=" mb-4">
+                The Kashmir markhor ( Capra falconeri ), a majestic wild goat species, is native to the mountainous regions of Kashmir and northern Pakistan. Recognizable by its spiraled horns and rugged appearance, it inhabits steep, rocky terrains of the Himalayas and Hindu Kush ranges. The species is classified as Near Threatened on the IUCN Red List, with populations fragmented due to habitat loss, poaching, and competition with livestock.
+                </p>
+                <img src="../assets/img/markhor.webp" alt="Blog image" class="img-fluid mb-4 w-100 object-fit-cover rounded-4"style="aspect-ratio:16/9"/>
+                <h4 class="fw-medium mb-4" id="traditional-and-medical-signifcance">
+                Traditional and Medicinal Significance
+                </h4>
+                <h5 class="fw-medium mb-4">Ethnomedicinal Uses</h5>
+                <p>In tribal communities of the Thakht-e-Sulaiman Hills (northwest Pakistan), the markhor has historically been hunted for its bile, meat, and skin.</p>
+                <ul class="mb-5">
+                    <li class="mb-3 fs-6"><strong class=" mb-3 fs-5">Bile:</strong> Used to treat hepatitis and liver disorders.</li>
+                    <li class="mb-3 fs-6"><strong class=" mb-3 fs-5">Skin:</strong> Worn as a medicinal remedy for ailments like arthritis and respiratory issues</li>
+                    <li class="mb-3 fs-6"><strong class="mb-3 fs-5">Horns:</strong>Sold as decorative items and trophies, contributing to illegal wildlife trade.</li>
+                   
+                </ul>
+                <h5 class="fw-medium mb-4">Cultural Practices</h5>
+                <p>Pashtun tribes and other local communities integrate markhor-derived products into traditional healing rituals, often passed down orally.</p>
+
+                <h4 class="fw-medium mb-4" id="conservation-challenges">
+                Conservation Challenges
+                </h4>
+                <ul class="mb-4 ">
+                    <li class="mb-3 fs-6"><strong class=" mb-3 fs-5">Poaching and Illegal Trade:</strong> High demand for markhor horns and bile in black markets drives unsustainable hunting.</li>
+                    <li class="mb-3 fs-6"><strong class="mb-3 fs-5">Habitat Fragmentation:</strong> Encroachment by livestock, deforestation, and infrastructure development reduce viable habitats.</li>
+                    <li class="mb-3 fs-6"><strong class="mb-3 fs-5">Human-Wildlife Conflict:</strong> Competition for grazing land exacerbates tensions between markhor populations and local herders.</li>
+                 
+                </ul>
+                <h4 class="fw-medium mb-4" id="conservation-efforts">
+                Conservation Efforts
+                </h4>
+                <ul class="mb-4 ">
+                    <li class="mb-3 fs-6"><strong class=" mb-3 fs-5">Protected Areas:</strong> The Kashmir markhor is found in protected regions like the Chitral Gol National Park and the Sulaiman Mountains.</li>
+                    <li class="mb-3 fs-6"><strong class="mb-3 fs-5">Community Engagement:</strong> Initiatives to involve local tribes in conservation, such as ecotourism and sustainable resource management, aim to reduce poaching.</li>
+                    <li class="mb-3 fs-6"><strong class="mb-3 fs-5">Legal Frameworks: </strong> Listed under Appendix I of CITES, prohibiting international trade. Pakistan’s Wildlife Act also enforces strict penalties for illegal hunting.
+                    </li>
+                 
+                </ul>
+               
+                
+            </div>
+            <div class="col-lg-4 ps-md-5">
+                <div class="sticky-top" style="top: 6rem;">
+                    <div class="mb-4">
+                        <h2 class="h5 fw-medium mb-3">
+                            In this article
+                        </h2>
+                        <ul class="list-unstyled">
+                            <li class="mb-2">
+                                <a class="" href="#markhor-in-kashmir">
+                                The Markhor in Kashmir
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a class="" href="#traditional-and-medical-signifcance">
+                                Traditional and Medicinal Significance
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a class="" href="#conservation-challenges">
+                                Conservation Challenges
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a class="" href="#conservation-efforts">
+                                Conservation Efforts
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mb-4">
+                        <h2 class="h5 fw-semibold mb-3">
+                            Author
+                        </h2>
+                        <div class="d-flex align-items-center">
+                            <img src="../assets/img/khalid-photo.webp" alt="Author's profile picture" class="rounded-circle me-3 object-fit-cover" width="50" height="50" />
+                            <div>
+                                <p class="fw-semibold mb-0">
+                                        KHALID ZAHOOR
+                                </p>
+                                <p class="text-secondary">
+                                    TRAVELLER
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="d-flex gap-3 align-items-center">
+                            <a class="btn btn-sm btn-outline-dark btn-sm-square rounded-circle me-2"
+                            href="https://twitter.com/khalidzahoor" target="_blank">
+                                <i class="fab fa-twitter fw-normal"></i>
+                            </a>
+                            <a class="btn btn-sm btn-outline-dark btn-sm-square rounded-circle me-2"
+                            href="https://facebook.com/khalidzahoor" target="_blank">
+                                <i class="fab fa-facebook-f fw-normal"></i>
+                            </a> 
+                            <a class="btn btn-sm btn-outline-dark btn-sm-square rounded-circle me-2"
+                            href="https://linkedin.com/in/khalidzahoor" target="_blank">
+                                <i class="fab fa-linkedin fw-normal"></i>
+                            </a> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <footer>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <script src="/assets/components/header.js"></script>
+    <script src="/assets/components/footer.js"></script>
+    <script src="/assets/js/header.js"></script>
+
+</body>
+
+</html>
