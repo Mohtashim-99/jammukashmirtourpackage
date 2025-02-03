3fb78198a486ff42ca1e5925b9eba9438337e8b1
Mohtashim-99 <mohtashimanayat63@gmail.com>
2025-02-03 21:57:26 +0530

    Added logo

--- assets/components/header.js
+++ assets/components/header.js
@@ -7,8 +7,8 @@ const topHeaderHTML = ` <div class="container-fluid bg-dark px-5 d-none d-lg-blo
                         </div>`;
 
 const navbarHTML = `<nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-                        <a href="" class="navbar-brand p-0">
-                            <h1 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>Tourist</h1>
+                        <a href="/" class="navbar-brand p-0">
+                                <img src="/assets/img/jammu-kashmir-tour-package-logo.png" alt="Logo">
                         </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarCollapse"><span class="fa fa-bars"></span></button>
