6e592f1c3fa913fd46c4226880a18628be38b07b
Mohtashim-99 <mohtashimanayat63@gmail.com>
2025-02-04 21:38:43 +0530

    Added light and dark logo

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,18 @@
 window.addEventListener('scroll', function () {
+    var logoA= document.querySelector('.logo-a');
     var navbar = document.querySelector('.navbar');
     if (window.scrollY > 45) {
         navbar.classList.add('sticky-top', 'shadow-sm');
     } else {
         navbar.classList.remove('sticky-top', 'shadow-sm');
     }
+
+    if(navbar.classList.contains('sticky-top')){
+        logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo.png" alt="Logo">';
+    }
+    else{
+        logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo-light.png" alt="Logo">';
+
+    }
 });
+
