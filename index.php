79073f359cace76be97db66948d95fa66fa45c1d
Mohtashim-99 <mohtashimanayat63@gmail.com>
2025-02-04 12:15:05 +0530

    Added absolute path

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ include ("app/controllers/lead_form.php");
 
     <link rel="stylesheet" href="assets/css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/bootstarp.min.css" />
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/css/bootstarp.min.css" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
     <script>
     document.addEventListener("DOMContentLoaded", function () {
         let navLinks = document.querySelectorAll(".navbar-nav .nav-link");
