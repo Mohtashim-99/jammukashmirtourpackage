67b59e5190d8e85cf52f853274b2737b801bc09f
Mohtashim-99 <mohtashimanayat63@gmail.com>
2025-02-16 18:59:48 +0530

    changed logo logic

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,5 @@
-window.addEventListener('scroll', function () {
-    var logoA= document.querySelector('.logo-a');
+let logoA= document.querySelector('.logo-a');
+window.addEventListener('scroll', function () { 
     var navbar = document.querySelector('.navbar');
     if (window.scrollY > 45) {
         navbar.classList.add('sticky-top', 'shadow-sm');
@@ -7,12 +7,23 @@ window.addEventListener('scroll', function () {
         navbar.classList.remove('sticky-top', 'shadow-sm');
     }
 
-    if(navbar.classList.contains('sticky-top')){
-        logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo.png" alt="Logo">';
+    if(screen.width>="991"){
+        if(navbar.classList.contains('sticky-top')){
+            logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo.png" alt="Logo">';
+        }
+        else{
+            logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo-light.png" alt="Logo">';
+        }
     }
-    else{
-        logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo-light.png" alt="Logo">';
-
+   else{
+    logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo.png" alt="Logo">';
+    
     }
+    
 });
+if(screen.width<="991"){
+    logoA.innerHTML='<img src="/assets/img/jammu-kashmir-tour-package-logo.png" alt="Logo">';
+}
+
+
 
