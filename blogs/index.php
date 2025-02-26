9578a021d08c55d799c6b843bdd645fbec75b90a
Mohtashim-99 <mohtashimanayat63@gmail.com>
2025-02-26 21:54:27 +0530

    Added touseem blog card

--- blogs/index.php
+++ blogs/index.php
@@ -17,23 +17,23 @@ include ("../app/controllers/lead_form.php");
     <link rel="stylesheet" href="/assets/css/bootstarp.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            let navLinks = document.querySelectorAll(".navbar-nav .nav-link");
-            let activePage = "Blogs"; // Change this for each page
-            navLinks.forEach(link => {
-                link.classList.remove("active"); // Remove active class from all links
-                if (link.textContent.trim() === activePage) {
-                    link.classList.add("active"); // Add active class to the specified link
-                }
-            });
+    document.addEventListener("DOMContentLoaded", function() {
+        let navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+        let activePage = "Blogs"; // Change this for each page
+        navLinks.forEach(link => {
+            link.classList.remove("active"); // Remove active class from all links
+            if (link.textContent.trim() === activePage) {
+                link.classList.add("active"); // Add active class to the specified link
+            }
         });
+    });
     </script>
 
 </head>
 
 <body>
-        <!-- Success Modal -->
-        <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+    <!-- Success Modal -->
+    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-success text-white">
@@ -85,7 +85,7 @@ include ("../app/controllers/lead_form.php");
             </div>
         </div>
     </div>
-    <div class="container-xxl py-5">
+    <div class="container-xxl py-5 px-0">
         <div class="container">
             <div class="text-center ">
                 <h6 class="section-title bg-white text-center text-primary px-3">Blogs</h6>
@@ -94,8 +94,8 @@ include ("../app/controllers/lead_form.php");
         </div>
 
         <div class="container my-5  blog-card">
-               <!-- From database -->
-            <div class="d-flex mb-4  flex-column mb-6">
+            <!-- From database -->
+            <div class="d-flex mb-5  flex-column">
                 <!-- Text Content -->
                 <div class="w-100">
                     <div class="d-flex flex-wrap gap-1 mb-2">
@@ -108,52 +108,87 @@ include ("../app/controllers/lead_form.php");
                 <div class="d-flex gap-4 flex-column flex-md-row">
                     <div class="w-100 w-md-50">
                         <p class="blog-description">
-                            Gulmarg, nestled in the Pir Panjal range of Jammu and Kashmir, transforms into a mesmerizing winter wonderland as the snow blankets its landscapes. Known as India's premier ski destination, Gulmarg offers a breathtaking experience for adventure seekers and nature lovers alike....
+                            Gulmarg, nestled in the Pir Panjal range of Jammu and Kashmir, transforms into a mesmerizing
+                            winter wonderland as the snow blankets its landscapes. Known as India's premier ski
+                            destination, Gulmarg offers a breathtaking experience for adventure seekers and nature
+                            lovers alike....
                         </p>
                         <a href="gulmarg-in-winters" class="read-more">Read more &rsaquo;</a>
                     </div>
 
 
                     <div class="w-100 w-md-50">
-                        <img src="/assets/img/gulamrg.jpg"
-                            alt="Blog Image" class="img-fluid rounded-3 object-fit-cover" style="aspect-ratio: 16/9;">
+                        <img src="/assets/img/gulamrg.jpg" alt="Blog Image" class="img-fluid rounded-3 object-fit-cover"
+                            style="aspect-ratio: 16/9;">
                     </div>
-            </div>
-            <div class="d-flex mb-4  flex-column mb-6">
-                <!-- Text Content -->
-                <div class="w-100">
-                    <div class="d-flex flex-wrap gap-1 mb-2">
-                        <div class="blog-category">Product Updates</div>
-                        <div class="blog-meta">FEB 02, 2025</div>
-                    </div>
-                    <h3 class="blog-title fw-500-important">The Markhor in Kashmir: Ecology, Conservation, and Cultural Significance</h3>
-
                 </div>
-                <div class="d-flex gap-4 flex-column flex-md-row">
-                    <div class="w-100 w-md-50">
-                        <p class="blog-description">
-                            The Kashmir markhor ( Capra falconeri ), a majestic wild goat species, is native to the mountainous regions of Kashmir and northern Pakistan. Recognizable by its spiraled horns and rugged appearance, it inhabits steep, rocky terrains of the Himalayas and Hindu Kush ranges. The species is classified as Near Threatened on the IUCN Red List, with populations fragmented due to habitat loss, poaching, and competition with livestock....
-                        </p>
-                        <a href="markhor-in-kashmir" class="read-more">Read more &rsaquo;</a>
+                </div>
+                <div class="d-flex mb-5  flex-column ">
+                    <!-- Text Content -->
+                    <div class="w-100">
+                        <div class="d-flex flex-wrap gap-1 mb-2">
+                            <div class="blog-category">Product Updates</div>
+                            <div class="blog-meta">FEB 02, 2025</div>
+                        </div>
+                        <h3 class="blog-title fw-500-important">The Markhor in Kashmir: Ecology, Conservation, and
+                            Cultural Significance</h3>
+
                     </div>
+                    <div class="d-flex gap-4 flex-column flex-md-row">
+                        <div class="w-100 w-md-50">
+                            <p class="blog-description">
+                                The Kashmir markhor ( Capra falconeri ), a majestic wild goat species, is native to the
+                                mountainous regions of Kashmir and northern Pakistan. Recognizable by its spiraled horns
+                                and rugged appearance, it inhabits steep, rocky terrains of the Himalayas and Hindu Kush
+                                ranges. The species is classified as Near Threatened on the IUCN Red List, with
+                                populations fragmented due to habitat loss, poaching, and competition with livestock....
+                            </p>
+                            <a href="markhor-in-kashmir" class="read-more">Read more &rsaquo;</a>
+                        </div>
 
 
-                    <div class="w-100 w-md-50">
-                        <img src="/assets/img/markhor.webp"
-                            alt="Blog Image" class="img-fluid rounded-3 object-fit-cover" style="aspect-ratio: 16/9;">
+                        <div class="w-100 w-md-50">
+                            <img src="/assets/img/markhor.webp" alt="Blog Image"
+                                class="img-fluid rounded-3 object-fit-cover" style="aspect-ratio: 16/9;">
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mb-5  flex-column ">
+                    <!-- Text Content -->
+                    <div class="w-100">
+                        <div class="d-flex flex-wrap gap-1 mb-2">
+                            <div class="blog-category">Product Updates</div>
+                            <div class="blog-meta">FEB 26, 2025</div>
+                        </div>
+                        <h3 class="blog-title fw-500-important">Exploring the Beauty of a Tulip Garden: Nature’s Vibrant Symphony</h3>
+
+                    </div>
+                    <div class="d-flex gap-4 flex-column flex-md-row">
+                        <div class="w-100 w-md-50">
+                            <p class="blog-description">
+                            Tulips, with their brilliant colors and elegant petals, are among the most cherished flowers in the world. They symbolize renewal and beauty, making them a perfect addition to gardens that celebrate the vibrancy of spring. Nowhere is this truer than in Kashmir, home to the breathtaking Indira Gandhi Memorial....
+                            </p>
+                            <a href="tulip-garden.php" class="read-more">Read more &rsaquo;</a>
+                        </div>
+
+
+                        <div class="w-100 w-md-50">
+                            <img src="/assets/img/tulip-garden.webp" alt="Blog Image"
+                                class="img-fluid rounded-3 object-fit-cover" style="aspect-ratio: 16/9;">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <footer>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
-    <script src="/assets/components/header.js"></script>
-    <script src="/assets/components/footer.js"></script>
-    <script src="/assets/js/header.js"></script>
+        </div>
+        <footer>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+        <script src="/assets/components/header.js"></script>
+        <script src="/assets/components/footer.js"></script>
+        <script src="/assets/js/header.js"></script>
 
 </body>
 
